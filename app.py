23ab4cc76cb4561cbe6f0daabdd9bd7a138f787c
DonatasNoreika <donoras@gmail.com>
2021-03-15 11:06:44 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+
 import os
 from flask import Flask, render_template, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
@@ -9,10 +10,9 @@ print(basedir)
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'dfgsfdgsdfgsdfgsdf'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'vaikaitevai.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-
 
 class Tevas(db.Model):
     __tablename__ = "tevas"
@@ -20,7 +20,7 @@ class Tevas(db.Model):
     vardas = db.Column("Vardas", db.String)
     pavarde = db.Column("Pavardė", db.String)
     vaikas_id = db.Column(db.Integer, db.ForeignKey("vaikas.id"))
-    vaikas = db.relationship("Vaikas")
+    vaikas = db.relationship("Vaikas", cascade="all, delete", passive_deletes=True)
 
 
 class Vaikas(db.Model):
@@ -28,10 +28,10 @@ class Vaikas(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     vardas = db.Column("Vardas", db.String)
     pavarde = db.Column("Pavardė", db.String)
-
+    tevai = db.relationship("Tevas")
 
 @app.route("/")
-def index():
+def home():
     return render_template("index.html")
 
 
@@ -53,6 +53,7 @@ def children():
     return render_template("vaikai.html", visi_vaikai=visi_vaikai)
 
 
+
 @app.route("/naujas_tevas", methods=["GET", "POST"])
 def new_parent():
     db.create_all()
@@ -65,25 +66,13 @@ def new_parent():
     return render_template("prideti_teva.html", form=forma)
 
 
-@app.route("/naujas_vaikas", methods=["GET", "POST"])
-def new_child():
-    db.create_all()
-    forma = forms.VaikasForm()
-    if forma.validate_on_submit():
-        naujas_vaikas = Vaikas(vardas=forma.vardas.data,
-                               pavarde=forma.pavarde.data)
-        db.session.add(naujas_vaikas)
-        db.session.commit()
-        return redirect(url_for('children'))
-    return render_template("prideti_vaika.html", form=forma)
-
-
 @app.route("/delete/<int:id>")
 def delete(id):
     uzklausa = Tevas.query.get(id)
     db.session.delete(uzklausa)
     db.session.commit()
     return redirect(url_for('parents'))
+
 
 @app.route("/update/<int:id>", methods=['GET', 'POST'])
 def update(id):
@@ -97,6 +86,24 @@ def update(id):
         return redirect(url_for('parents'))
     return render_template("update.html", form=form, tevas=tevas)
 
+@app.route("/naujas_vaikas", methods=["GET", "POST"])
+def new_child():
+    db.create_all()
+    forma = forms.VaikasForm()
+    if forma.validate_on_submit():
+        naujas_vaikas = Vaikas(vardas=forma.vardas.data,
+                               pavarde=forma.pavarde.data)
+        db.session.add(naujas_vaikas)
+        db.session.commit()
+        return redirect(url_for('children'))
+    return render_template("prideti_vaika.html", form=forma)
+
+@app.route("/vaikas_delete/<int:id>")
+def vaikas_delete(id):
+    uzklausa = Vaikas.query.get(id)
+    db.session.delete(uzklausa)
+    db.session.commit()
+    return redirect(url_for('children'))
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8000, debug=True)
